eea6fdb9259a2279da0f37e40cbd3ff300d1da66
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-09-03 13:31:43 +0000

    Update installation script

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -26,4 +26,5 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 #install compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
