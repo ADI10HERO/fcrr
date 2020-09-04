594ddf15e61a8209f9c41e9751c2094ec11d1162
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-09-04 11:45:01 +0530

    Docs: Docstrings added, add pep8 formating

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# Scalable flask server to handle async long tasks
+# using Flask, celery, rabitmq and redis.
+#
+# (C) 2020 Aditya Srivastava, Mumbai, India
+# GNU General Public License v3 (GPLv3)
+# email adityasrivastava301199@gmail.com
+
 from flask import Flask, flash, render_template, request, redirect, url_for
 from celery import Celery
 from time import sleep, time
@@ -19,19 +26,36 @@ client.conf.update(app.config)
 
 r = redis.Redis(host='redis')
 
+
 def generate_ts():
+    """Generate timestamp string."""
     return ''.join(str(time()).split('.'))
+
 
 @client.task(bind=True)
 def long_task(self, task_id, url):
+    """
+    Simulate a long task.
 
+    Inputs:
+    task_id: string, unique hash of each task
+    url: string, the url of audio file to download
+
+    Return:
+    json serializable object, status of the task after completion
+    """
     db_contents = {
-                    'state' : 'progress', 
-                    'url': url, 
-                    'message': 'your file is being processed now'
-                }
+        'state': 'progress',
+        'url': url,
+        'message': 'your file is being processed now'
+    }
     r.hmset(task_id, db_contents)
-    self.update_state(state='PROGRESS', meta={'current': 0, 'total': 1, 'status': "task is in progress"})
+    self.update_state(state='PROGRESS',
+                      meta={
+                          'current': 0,
+                          'total': 1,
+                          'status': "task is in progress"
+                      })
 
     try:
         rq = requests.get(url)
@@ -40,30 +64,36 @@ def long_task(self, task_id, url):
         with open(fpath, "wb") as f:
             f.write(rq.content)
         sleep(40)
-        os.remove(fpath) # deleting the temp file 
+        os.remove(fpath)  # deleting the temp file
         db_contents = {
-                    'state' : 'success', 
-                    'url': url, 
-                    'message': 'processing completed'
-                }
+            'state': 'success',
+            'url': url,
+            'message': 'processing completed'
+        }
         r.hmset(task_id, db_contents)
         r.expire(task_id, 60*60*24)
 
     except Exception as e:
         db_contents = {
-                        'state' : 'Failed', 
-                        'url': url, 
-                        'message': e,
-                    }
+            'state': 'Failed',
+            'url': url,
+            'message': e,
+        }
         # print(e)
         r.hmset(task_id, db_contents)
         r.expire(task_id, 60*60*24)
-        #return {"task-status": "exception"}
+        # return {"task-status": "exception"}
 
     return {"Task-Status": "Completed"}
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    Route to root/home page.
+
+    Accepts get and post requests
+    """
     if request.method == 'GET':
         return render_template('index.html')
 
@@ -73,43 +103,50 @@ def index():
         result = r.hgetall(task_id)
 
         if result:
-            # result exists, i.e previously queried 
-            msg = str(r.hget(task_id, "state"))
+            # result exists, i.e previously queried
+            msg = str(task_id) + " " + r.hget(task_id, "state").decode('utf-8')
+            info = "Previous request found, skipped download,\
+                    returning result"
             if msg != "Failed":
-                return render_template('index.html', message = msg)
+                return render_template('index.html', message=msg, info=info)
 
         # create task with task_id as id
         db_contents = {
-                        'state' : 'accepted', 
-                        'url': url, 
-                        'message': 'your file is accepted, will be processed soon'
-                    }
+            'state': 'accepted',
+            'url': url,
+            'message': 'your file is accepted, will be processed soon'
+        }
         r.hmset(task_id, db_contents)
 
-        # finally call the long task 
+        # finally call the long task
         longtask = long_task.delay(task_id, url)
-        return render_template('index.html', message = task_id)
+        return render_template('index.html', message=task_id)
+
 
 @app.route('/status/<id>', methods=['GET'])
 def check_status(id):
-    
+    """Route to check the status of the long async task."""
     result = r.hgetall(id)
     if result is None:
-        # result does not exist 
-        return render_template('status.html', status = "Not Found", info = "No such task found")
-    
+        # result does not exist
+        return render_template('status.html',
+                               status="Not Found",
+                               info="No such task found"
+                               )
+
     task_id = id
     status = r.hget(task_id, "state").decode('utf-8')
     info = r.hget(task_id, "message").decode('utf-8')
 
-    return render_template('status.html', status = str(status), info = str(info))
+    return render_template('status.html', status=str(status), info=str(info))
+
 
 @app.route('/clear', methods=['GET'])
 def clear():
+    """Helper function to clear the redis-db."""
     r.flushdb()
     return "db cleared"
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
